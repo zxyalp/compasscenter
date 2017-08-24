32ecdf58a339c7e346fd4c7e301f2b7d1f93fef4
xing yan <zgyan012@gmail.com>
2017-08-24 21:17:20 +0800

    first commit

---
+++ src/main/java/com/alpimzy/App.java
@@ -0,0 +1,13 @@
+package com.alpimzy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
